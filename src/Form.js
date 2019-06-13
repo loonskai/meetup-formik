28b94991b3784e149f99316bd7c5ca21633a91de
Siarhei Lunski <the.sergaunt@gmail.com>
2019-06-13 22:21:44 +0300

    Init useFormik with debugger window

--- src/Form.js
+++ src/Form.js
@@ -1,26 +1,65 @@
 import React, { useReducer } from 'react';
+import { SET_FIELD_VALUE } from './constants';
 
-const initialState = {
-  email: '',
-  username: '',
-  password: ''
-};
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case SET_FIELD_VALUE:
+      return {
+        ...state, // contains errors and touched
+        values: {
+          ...state.values,
+          ...payload
+        }
+      };
+    default:
+      return state;
+  }
+}
 
-function Form() {
-  const [state, setState] = useReducer(
-    (prevState, updatedValues) => ({ ...prevState, ...updatedValues }),
-    initialState
-  );
+function useFormik({ initialValues }) {
+  // As our useFormik internal state is complex object with some nested properties we'll use useReducer hook instead of useState. Another point is that in more complex form it's much easier to handle all the inputs using reducer concept
+  // Formik uses name attribute of inputs as the way to access symmetrical aspects of useFormik state: values.name, errors.name, touched.name. That's pretty scalable
+  const [state, dispatch] = useReducer(reducer, {
+    values: initialValues,
+    errors: {},
+    touched: {}
+  });
 
-  const handleChange = ({ target: { name, value } }) =>
-    setState({
-      [name]: value
+  // When we run handleChange from our component it's gonna run dispatch SET_FIELD_VALUE
+  const handleChange = event => {
+    event.persist();
+    const {
+      target: { name, value }
+    } = event;
+    dispatch({
+      type: SET_FIELD_VALUE,
+      payload: {
+        [name]: value
+      }
     });
+  };
 
-  const handleSubmit = e => {
+  /*  const handleSubmit = e => {
     e.preventDefault();
     console.log(state);
+  }; */
+
+  return {
+    handleChange,
+    state
   };
+}
+
+function Form() {
+  const options = {
+    initialValues: {
+      email: '',
+      username: '',
+      password: ''
+    }
+  };
+  const formik = useFormik(options);
+  const { handleSubmit, handleChange, ...state } = formik;
 
   return (
     <form onSubmit={handleSubmit}>
@@ -48,6 +87,13 @@ function Form() {
         name="password"
         autoComplete="off"
       />
+      <div className="debugger">
+        {Object.keys(state.state.values).map(key => (
+          <p key={key}>
+            {key}: "{state.state.values[key]}"
+          </p>
+        ))}
+      </div>
       <button type="submit">Sign up</button>
     </form>
   );
